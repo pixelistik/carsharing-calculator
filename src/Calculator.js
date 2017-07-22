1959951595ea808f2dc4d9fa09f442b36699f015
Pixelistik <code@pixelistik.de>
2017-07-23 00:19:04 +0200

    Multiple tariffs, returned by cost

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,17 +1,51 @@
+const _ = require('lodash');
+
+const defaultCostCalculation = function defaultCostCalculation(driving, parking) {
+  return (this.drivingPerMinute * driving) + (this.parkingPerMinute * parking);
+};
+
 const Calculator = {
   tariffs: {
     car2go: {
       smart: {
         drivingPerMinute: 0.24,
         parkingPerMinute: 0.19,
+        calculateCost: defaultCostCalculation,
+      },
+    },
+    driveNow: {
+      mini: {
+        drivingPerMinute: 0.31,
+        parkingPerMinute: 0.15,
+        calculateCost: defaultCostCalculation,
+      },
+      bmw2: {
+        drivingPerMinute: 0.34,
+        parkingPerMinute: 0.15,
+        calculateCost: defaultCostCalculation,
       },
     },
   },
-  cost: (params) => {
-    const drivingCost = params.tariff.drivingPerMinute * params.driving;
-    const parkingCost = params.tariff.parkingPerMinute * params.parking;
+  calculateAllCosts: function calculateAllCosts(driving, parking) {
+    const tariffs = [];
 
-    return drivingCost + parkingCost;
+    let keys = Object.keys(this.tariffs.car2go);
+    keys.forEach((key) => {
+      tariffs.push(this.tariffs.car2go[key]);
+    });
+
+    keys = Object.keys(this.tariffs.driveNow);
+    keys.forEach((key) => {
+      tariffs.push(this.tariffs.driveNow[key]);
+    });
+
+    const costs = tariffs.map((tariff) => {
+      const tariffWithCost = tariff;
+      tariffWithCost.totalCost = tariff.calculateCost(driving, parking);
+      return tariffWithCost;
+    });
+
+    return _.sortBy(costs, ['cost']);
   },
 };
 
--- test/unit/specs/Calculator.spec.js
+++ test/unit/specs/Calculator.spec.js
@@ -11,24 +11,24 @@ describe('Calculator', () => {
   describe('Car2Go', () => {
     it('should make a simple driving calculation', () => {
       const calculator = Object.create(Calculator);
-      const result = calculator.cost({
-        tariff: calculator.tariffs.car2go.smart,
-        driving: 10,
-        parking: 0,
-      });
-
+      const result = calculator.tariffs.car2go.smart.calculateCost(10, 0);
       expect(result).to.equal(0.24 * 10);
     });
 
     it('should make a driving/parking calculation', () => {
       const calculator = Object.create(Calculator);
-      const result = calculator.cost({
-        tariff: calculator.tariffs.car2go.smart,
-        driving: 10,
-        parking: 5,
-      });
-
+      const result = calculator.tariffs.car2go.smart.calculateCost(10, 5);
       expect(result).to.equal((0.24 * 10) + (0.19 * 5));
+    });
+
+    it('should list all the tariffs', () => {
+      const calculator = Object.create(Calculator);
+      const result = calculator.calculateAllCosts(1, 0);
+
+      expect(result).to.have.length(3);
+      expect(result[0].totalCost).to.equal(0.24);
+      expect(result[1].totalCost).to.equal(0.31);
+      expect(result[2].totalCost).to.equal(0.34);
     });
   });
 });
