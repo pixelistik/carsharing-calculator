e50e32e2039eec72c9ae46ef6ece612bb893a704
Pixelistik <code@pixelistik.de>
2017-07-24 22:42:22 +0200

    The test is not car2go-specific

--- test/unit/specs/Calculator.spec.js
+++ test/unit/specs/Calculator.spec.js
@@ -12,6 +12,15 @@ describe('Calculator', () => {
       .to.be.an('object');
   });
 
+  it('should list all the tariffs', () => {
+    const result = calculator.calculateAllCosts(1, 0);
+
+    expect(result).to.have.length(3);
+    expect(result[0].totalCost).to.equal(0.24);
+    expect(result[1].totalCost).to.equal(0.31);
+    expect(result[2].totalCost).to.equal(0.34);
+  });
+
   describe('Car2Go', () => {
     it('should make a simple driving calculation', () => {
       const result = calculator.tariffs.car2go.smart.calculateCost(10, 0);
@@ -21,15 +30,6 @@ describe('Calculator', () => {
     it('should make a driving/parking calculation', () => {
       const result = calculator.tariffs.car2go.smart.calculateCost(10, 5);
       expect(result).to.equal((0.24 * 10) + (0.19 * 5));
-    });
-
-    it('should list all the tariffs', () => {
-      const result = calculator.calculateAllCosts(1, 0);
-
-      expect(result).to.have.length(3);
-      expect(result[0].totalCost).to.equal(0.24);
-      expect(result[1].totalCost).to.equal(0.31);
-      expect(result[2].totalCost).to.equal(0.34);
     });
   });
 });
