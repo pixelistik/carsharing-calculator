dcc79cc6250fb309737b513cba9928ad0f912e98
Pixelistik <code@pixelistik.de>
2017-07-24 22:41:30 +0200

    Extract test instance creation, DRY

--- test/unit/specs/Calculator.spec.js
+++ test/unit/specs/Calculator.spec.js
@@ -1,28 +1,29 @@
 import Calculator from '@/Calculator';
 
 describe('Calculator', () => {
-  it('should instantiate', () => {
-    const calculator = Object.create(Calculator);
+  let calculator;
 
+  beforeEach(() => {
+    calculator = Object.create(Calculator);
+  });
+
+  it('should instantiate', () => {
     expect(calculator)
       .to.be.an('object');
   });
 
   describe('Car2Go', () => {
     it('should make a simple driving calculation', () => {
-      const calculator = Object.create(Calculator);
       const result = calculator.tariffs.car2go.smart.calculateCost(10, 0);
       expect(result).to.equal(0.24 * 10);
     });
 
     it('should make a driving/parking calculation', () => {
-      const calculator = Object.create(Calculator);
       const result = calculator.tariffs.car2go.smart.calculateCost(10, 5);
       expect(result).to.equal((0.24 * 10) + (0.19 * 5));
     });
 
     it('should list all the tariffs', () => {
-      const calculator = Object.create(Calculator);
       const result = calculator.calculateAllCosts(1, 0);
 
       expect(result).to.have.length(3);
