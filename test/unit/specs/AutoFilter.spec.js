954adee4e0577b85a2f9a6ccf3af30fad023afb2
Pixelistik <code@pixelistik.de>
2017-08-04 13:36:52 +0200

    Split up tests into meaningful cases

--- test/unit/specs/AutoFilter.spec.js
+++ test/unit/specs/AutoFilter.spec.js
@@ -2,36 +2,44 @@ import Vue from 'vue';
 import AutoFilter from '@/components/AutoFilter';
 
 describe('AutoFilter.vue', () => {
-  it('should render correct contents', () => {
-    const Constructor = Vue.extend(AutoFilter);
-    const vm = new Constructor({
+  const Constructor = Vue.extend(AutoFilter);
+  let vm;
+
+  const itemsFixture = [
+    {
+      id: 1,
+      filterProperties: {
+        doors: 2,
+        engine: 'diesel',
+      },
+    },
+    {
+      id: 2,
+      filterProperties: {
+        doors: 3,
+        engine: 'diesel',
+      },
+    },
+  ];
+
+  beforeEach(() => {
+    vm = new Constructor({
       propsData: {
-        items: [
-          {
-            id: 1,
-            filterProperties: {
-              doors: 2,
-              engine: 'diesel',
-            },
-          },
-          {
-            id: 2,
-            filterProperties: {
-              doors: 3,
-              engine: 'diesel',
-            },
-          },
-        ],
+        items: itemsFixture,
       },
     }).$mount();
+  });
 
+  it('should render the collected categories', () => {
     expect(
       vm.$el.querySelectorAll('.filter--category .filter--category--name')[0].textContent.trim(),
     ).to.equal('doors');
     expect(
       vm.$el.querySelectorAll('.filter--category .filter--category--name')[1].textContent.trim(),
     ).to.equal('engine');
+  });
 
+  it('should render the collected values of the first category', () => {
     expect(
       vm.$el
         .querySelectorAll('.filter--category:nth-child(1) .filter--value')[0]
@@ -43,7 +51,9 @@ describe('AutoFilter.vue', () => {
         .querySelectorAll('.filter--category:nth-child(1) .filter--value')[1]
         .textContent.trim(),
     ).to.equal('3');
+  });
 
+  it('should de-ducplicate the collected value to two unique ones', () => {
     expect(
       vm.$el
         .querySelectorAll('.filter--category:nth-child(2) .filter--value').length,
