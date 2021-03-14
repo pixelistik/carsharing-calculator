d2998c635ccfd4f8aeaf42a1cb7c6782cd1a8863
Pixelistik <code@pixelistik.de>
2021-03-14 12:00:44 +0100

    Import Vue Material CSS, too

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue';
 import App from './App';
 
 import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 Vue.use(VueMaterial);
 
 Vue.config.productionTip = false;
